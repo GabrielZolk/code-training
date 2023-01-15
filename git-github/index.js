b6daa826692393b912b070c42282d29ed64b8894
GabrielEagle <dialixkat@gmail.com>
2023-01-14 22:54:10 -0300

    git-github workflow

---
+++ git-github/index.js
@@ -0,0 +1,39 @@
+// STAGE 1 = UNTRACKED
+
+// git add .
+
+// STAGE 2 = STAGED
+
+// git commit
+
+// STAGE 3 = LOCAL (GIT)
+
+// git push
+
+// STAGE 4 = REMOTE (GITHUB)
+
+
+//--------------------------------------------------
+
+// git init
+
+// git branch -m [NAME OF BRANCH]
+
+// git remote add origin git@github.com:[GIT USERNAME]/[REPOSITORY NAME].git
+
+// git checkout -b [NAME OF BRANCH]
+
+// git checkout [NAME OF BRANCH]
+
+// git pull --rebase origin main
+
+// git rebase --continue
+
+// :wq (write quite)
+
+// git push -f
+
+// git pull
+
+// git log
+
